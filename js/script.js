af275a38c1e17efb10102dcd809f1c36312d5dc8
EllenBessa <ellenbessa96@gmail.com>
2023-03-23 19:42:22 -0300

    feat: :lipstick: Galeria imagens em javascript

--- js/script.js
+++ js/script.js
@@ -45,3 +45,22 @@ function eventoPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventoPerguntas);
+
+// Galeria de bicicleta
+
+const galeria = document.querySelectorAll(".bicicleta-img img");
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventoGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventoGaleria);
